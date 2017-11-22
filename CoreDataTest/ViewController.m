9dd131cf3fb62bcd2dec5436ae4e0c5990619f43
郑晗 <zhenghan@mxth.com>
2017-11-22 17:07:05 +0800

    han
    1

--- CoreDataTest/ViewController.m
+++ CoreDataTest/ViewController.m
@@ -36,23 +36,130 @@
 }
 
 #pragma mark tableViewDataSource and Delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return self.fetchedResultC.sections.count;
+}
+
+
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return self.dataArr.count;
+    return self.fetchedResultC.sections[section].numberOfObjects;
+    
+//    School *schoolModel = [self.fetchedResultC.fetchedObjects objectAtIndex:section];
+//    return schoolModel.student.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     StudentCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath] ;
-    
-    cell.studentModel = self.dataArr[indexPath.row];
+//    School *schoolModel = [self.fetchedResultC.fetchedObjects objectAtIndex:indexPath.section];
+    cell.studentModel = [self.fetchedResultC objectAtIndexPath:indexPath];
     return cell;
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
 
+/*
+ 在删除cell后在tableView代理方法的回调中，调用了MOC的删除方法，使本地持久化存储和UI保持同步，并回调到下面的FRC代理方法中，在代理方法中对UI做删除操作，这样一套由UI的改变引发的删除流程就完成了。
+ */
+
+//简单模拟 UI删除cell后，本地持久化区数据和UI同步的操作。在调用下面MOC保存上下文方法后，FRC会回调代理方法并更新UI
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        Student *tempModel = [self.fetchedResultC objectAtIndexPath:indexPath];
+        [self.context deleteObject:tempModel];
+        NSError *error = nil;
+        if (![self.context save:&error]) {
+             NSLog(@"tableView delete cell error : %@", error);
+        }
+    }
+}
+
+
+#pragma mark fetched results controller delegate
+//cell数据源发生改变会回调此方法
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath{
+    switch (type) {
+        case NSFetchedResultsChangeInsert:
+        {
+            [self.mainTable insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+            
+            break;
+           
+        case NSFetchedResultsChangeDelete:
+        {
+            [self.mainTable deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+            break;
+            
+        case NSFetchedResultsChangeMove:
+        {
+            
+            [self.mainTable deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            [self.mainTable insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+        {
+            StudentCell *cell = [self.mainTable cellForRowAtIndexPath:indexPath];
+            Student *tempModel = [self.fetchedResultC objectAtIndexPath:indexPath];
+            cell.studentModel = tempModel;
+        }
+            break;
+            
+            
+            
+        default:
+            break;
+    }
+}
+
+//section的数据源发生改变 回调该方法
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id<NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type{
+    switch (type) {
+        case NSFetchedResultsChangeInsert:
+            [self.mainTable insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+//本地数据源发生改变，将要开始回调FRC代理方法
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller{
+    if (@available(iOS 11.0, *)) {
+        [self.mainTable performBatchUpdates:^{
+            
+        } completion:^(BOOL finished) {
+            
+        }];
+    } else {
+        // Fallback on earlier versions
+        
+        [self.mainTable beginUpdates];
+    }
+    
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller{
+    [self.mainTable endUpdates];
+}
+
+// 返回section的title，可以在这里对title做进一步处理。这里修改title后，对应section的indexTitle属性会被更新。
+- (nullable NSString *)controller:(NSFetchedResultsController *)controller sectionIndexTitleForSectionName:(NSString *)sectionName{
+    return sectionName;
+}
+
+#pragma mark event
 - (IBAction)addData:(UIButton *)sender {
-   //创建托管对象，并指明创建的托管对象所属实体名
+   //创建托管对象，并指明创建的托管对象所属实体名
     School *school = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([School class]) inManagedObjectContext:self.context];
     school.name = @"实验小学";
-    
+    school.historyYear = 50;
     
     ClassAndGrade *grade = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([ClassAndGrade class])  inManagedObjectContext:self.context];
     grade.grade = @"五年级";
@@ -117,24 +224,14 @@
 - (IBAction)getAllData:(id)sender {
     //NSFetchedResultsController使用
     
-    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"School"];
-    
+    //执行获取请求，执行后FRC会从持久化存储区加载数据，其他地方可以通过FRC获取数据
+    //调用performFetch:方法第一次获取到数据并不会回调FRC代理方法。
     NSError *error = nil;
-    
-    
-    NSArray<School *> *schoolData = [self.context executeFetchRequest:request error:&error];
-    __block NSMutableArray *tempArr = [NSMutableArray arrayWithCapacity:0];
-    [schoolData enumerateObjectsUsingBlock:^(School * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        NSSortDescriptor *descp = [[NSSortDescriptor alloc] initWithKey:@"studentId" ascending:YES];
-        NSArray *descpArr = [NSArray arrayWithObject:descp];
-        NSArray *sortArr = [obj.student sortedArrayUsingDescriptors:descpArr];
-        [tempArr addObjectsFromArray:sortArr];
-        self.dataArr = [NSArray arrayWithArray:tempArr];
-        [self.mainTable reloadData];
-        
-        
-    }];
-    
+    [self.fetchedResultC performFetch:&error];
+    if (error) {
+        NSLog(@"NSFetchedResultsController init error : %@", error);
+    }
+    [self.mainTable reloadData];
 }
 
 - (IBAction)deleteData:(id)sender {
@@ -148,8 +245,6 @@
     //执行获取操作，找到要删除的对象
     NSError *error = nil;
    NSArray<School *> *tempArr =  [self.context executeFetchRequest:request error:&error];
-    
-    
     
     [tempArr enumerateObjectsUsingBlock:^(School * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         [self.context deleteObject:obj];
@@ -217,20 +312,16 @@
 - (NSFetchedResultsController *)fetchedResultC{
     if (_fetchedResultC == nil) {
         //创建请求对象，并指明操作的表
-        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"School"];
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Student"];
         //设置排序规则
-        NSSortDescriptor *descriptor = [NSSortDescriptor sortDescriptorWithKey:@"student.studentId" ascending:YES];
+        NSSortDescriptor *descriptor = [NSSortDescriptor sortDescriptorWithKey:@"studentId" ascending:YES];
         request.sortDescriptors = @[descriptor];
-        
+
         //创建NSFetchedResultsController控制器实例，并绑定MOC
-        NSError *error = nil;
-        _fetchedResultC = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:self.context sectionNameKeyPath:@"teacher.name" cacheName:nil];
+        
+        _fetchedResultC = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:self.context sectionNameKeyPath:nil cacheName:nil];//@"teacher.name"
         _fetchedResultC.delegate = self;
-        //执行获取请求，执行后FRC会从持久化存储区加载数据，其他地方可以通过FRC获取数据
-        [_fetchedResultC performFetch:&error];
-        if (error) {
-             NSLog(@"NSFetchedResultsController init error : %@", error);
-        }
+        
     }
     return _fetchedResultC;
 }
